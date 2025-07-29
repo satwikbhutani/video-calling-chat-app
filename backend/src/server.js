32f98300f159d8f696a870f33a675b3923529877
Satwik Bhutani <154877213+satwikbhutani@users.noreply.github.com>
2025-07-29 16:04:51 +0530

    Changes to make application deployment ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,12 @@ import chatRoutes from "./routes/chat.route.js"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import path from "path"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 dotenv.config()
 
 const app=express()
@@ -21,6 +27,15 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
 app.use("/api/chat", chatRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+  });
+}
+
 app.listen(PORT,()=>{
     connectDB()
     console.log(`server is running on the port ${PORT}`)
