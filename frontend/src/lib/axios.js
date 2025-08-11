c05801e456c7ee2a3a7cd38aa61b742cf2858992
Satwik Bhutani <154877213+satwikbhutani@users.noreply.github.com>
2025-08-11 17:54:50 +0530

    update BASE_URL for production environment in axios configuration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://unmute-eqf5.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: "http://localhost:5001/api",
